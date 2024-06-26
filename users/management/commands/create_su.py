14a11a60e8def68bfad42e6db494dbf0a18dad24
FairuzovR <skyfairrr@gmail.com>
2024-06-27 00:20:44 +0500

    Добавление команды для создания суперпользователя

--- users/management/commands/create_su.py
+++ users/management/commands/create_su.py
@@ -1,15 +1,41 @@
-from ...models import User
 from django.core.management.base import BaseCommand
+from ...models import User
 
 
 class Command(BaseCommand):
-    """
-    Создание суперпользователя
-    """
+    help = 'Create a superuser with specified credentials'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--email',
+            type=str,
+            help='Email address of the superuser'
+        )
+        parser.add_argument(
+            '--password',
+            type=str,
+            help='Password for the superuser'
+        )
+
     def handle(self, *args, **options):
-        user = User.objects.create()
-        user.username = 'SU_CMD_2'
-        user.email = 'superuser@su.su'
-        user.set_password('12345')
+        email = options['email']
+        password = options['password']
+
+        if not email or not password:
+            self.stdout.write(self.style.ERROR(
+                    'Error: Email and password are required'
+                ))
+            return
+
+        if User.objects.filter(email=email).exists():
+            self.stdout.write(self.style.ERROR(
+                f'Error: User with email "{email}" already exists'
+            ))
+            return
+
+        user = User.objects.create(email=email, password=password)
         user.is_superuser = True
-        user.is_staff = True
+        user.save()
+        self.stdout.write(
+            self.style.SUCCESS(f'Superuser "{email}" created successfully')
+        )
